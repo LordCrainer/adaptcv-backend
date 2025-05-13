9cf6332682120668ad43d1a1de3d4ef8a612fb47
jbarcos-blaze <jose.gonzales@blaze.com>
2025-05-12 23:10:29 -0500

    feat: add BuilderRepository class extending BaseRepository for Builder model

---
+++ src/api/Builder/builder.repository.ts
@@ -0,0 +1,10 @@
+import type { Builder } from '@lordcrainer/adaptcv-shared-types'
+
+import { BaseRepository } from '@src/Shared/utils/base.repository'
+import { BuilderModel } from './repository/builder.schema'
+
+export class BuilderRepository extends BaseRepository<Builder> {
+  constructor() {
+    super(BuilderModel, 'mongo')
+  }
+}
