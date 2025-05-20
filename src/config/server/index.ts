c40f575d268e4c1d782695f1f8337e860130ef24
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-05-20 09:15:54 -0500

    feat: add Swagger documentation setup and update route handling

--- src/config/server/index.ts
+++ src/config/server/index.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 
 import env from '@src/config/environments'
+import currentEnv from '@src/config/environments'
 import { expressMiddleware } from '@src/config/server/express.middleware'
 import { errorHandler } from '@src/middleware/error.handler.http'
+import swaggerRouter from '@src/routes/swaggerConfig'
 import { routerV1 } from '@src/routes/v1/index'
+import { customError } from '@src/Shared/utils/errorUtils'
 
 const app: express.Application = express()
 app.set('port', env.server.port)
@@ -11,7 +14,23 @@ app.set('secret', env.secret)
 app.set('trust proxy', env.configGeneral?.trustProxy)
 
 expressMiddleware(app)
+app.use('/docs', swaggerRouter)
+
+app.get('/', (req, res, next) => {
+  if (currentEnv.environment === 'production') {
+    next(customError('notFound', 'Not Found Route'))
+  } else {
+    res.redirect('/docs')
+  }
+})
+
 app.use('/v1', routerV1)
+
+app.use((req, res, next) => {
+  next(
+    customError('notFound', `Not Found Route: ${req.method} ${req.originalUrl}`)
+  )
+})
 
 app.use(errorHandler)
 
