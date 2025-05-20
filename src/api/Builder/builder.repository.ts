62e2f78bf1dc81ca09795f8c8aa78af21ea09612
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-05-20 10:45:18 -0500

    refactor: update BuilderRepository to use IBuilder type

--- src/api/Builder/builder.repository.ts
+++ src/api/Builder/builder.repository.ts
@@ -3,7 +3,7 @@ import type { IBuilder } from '@lordcrainer/adaptcv-shared-types'
 import { BaseRepository } from '@src/Shared/utils/base.repository'
 import { BuilderModel } from './repository/builder.schema'
 
-export class BuilderRepository extends BaseRepository<Builder> {
+export class BuilderRepository extends BaseRepository<IBuilder> {
   constructor() {
     super(BuilderModel, 'mongo')
   }
