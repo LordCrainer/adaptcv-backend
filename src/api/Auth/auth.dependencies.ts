67bcd4a2768424b6dc19e78d9041d35542e41001
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-06-07 16:52:01 -0500

    refactor: update registration service import and remove unused instance

--- src/api/Auth/auth.dependencies.ts
+++ src/api/Auth/auth.dependencies.ts
@@ -1,6 +1,6 @@
 import { AuthMiddleware } from '@src/middleware/auth.middleware'
 import { UserRepositoryMongo } from '@Api/Users/repository/users.repository.mongo'
-import { RegistrationService } from '@src/services/registration/registration.service'
+import { registrationService } from '@src/services/registration/registration.dependencies'
 import { userService } from '../Users/users.dependencies'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
@@ -8,7 +8,6 @@ import { AuthService } from './auth.service'
 const userRepository = new UserRepositoryMongo()
 
 export const authService = new AuthService(userRepository)
-export const registrationService = new RegistrationService(userRepository)
 
 export const inyectAuthController = new AuthController(authService, registrationService)
 
