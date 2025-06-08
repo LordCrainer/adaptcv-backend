d21607a90ee4894978da0c475f7fd02c4aef466f
LordCrainer <carlosgarcia.cagm@gmail.com>
2025-06-08 09:32:24 -0500

    refactor(user): [acv-3] Update user permission rules to use specific view permissions for getUser and getUsers

--- src/api/Users/permissions/users.permissions.ts
+++ src/api/Users/permissions/users.permissions.ts
@@ -1,5 +1,6 @@
 import type { PermissionMethod } from '@src/middleware/permissions.middleware'
 
+import { Roles } from '@src/api/Roles/roles'
 import {
   basePermissionRules,
   checkPermissions
@@ -12,9 +13,13 @@ type UserPermissionType =
   | 'deleteUser'
   | 'updateUser'
 
+const viewUserPermission: PermissionMethod = (params) => {
+  return Roles.isSuperAdmin(params.role) || Roles.isUser(params.role)
+}
+
 const userPermissionRules: Record<UserPermissionType, PermissionMethod> = {
-  getUser: basePermissionRules,
-  getUsers: basePermissionRules,
+  getUser: viewUserPermission,
+  getUsers: viewUserPermission,
   createUser: basePermissionRules,
   deleteUser: basePermissionRules,
   updateUser: basePermissionRules
